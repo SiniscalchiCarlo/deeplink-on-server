3b901bce22034fd9035ce168ec1727633e860423
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-12-09 16:06:48 +0300

    link added for connect app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/.well-known', express.static('.well-known'));
 
 // Routes;
 app.get('/', (req, res) => {
